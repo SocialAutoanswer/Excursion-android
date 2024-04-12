4bbadaf2a7c5c279371c2f9a9ced617d3b2f77d8
mason10q <moskvichev610@gmail.com>
2024-04-13 02:59:44 +0300

    Added splash screen

--- app/src/main/java/ru/exursion/MainActivity.kt
+++ app/src/main/java/ru/exursion/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.animation.AnticipateInterpolator
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import ru.exursion.databinding.ActivityMainBinding
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        configureSplashScreen()
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         with(binding) {
@@ -29,28 +30,5 @@ class MainActivity : AppCompatActivity() {
             mainBottomNavigation.setupWithNavController(findNavController(R.id.nav_container))
 
         }
-    }
-
-    @SuppressLint("NewApi")
-    private fun configureSplashScreen() {
-
-        splashScreen.setSplashScreenTheme(R.style.Theme_App_Starting)
-
-        splashScreen.setOnExitAnimationListener { splashScreenView ->
-            ObjectAnimator.ofFloat(
-                splashScreenView,
-                View.TRANSLATION_Y,
-                0f,
-                splashScreenView.height.toFloat()
-            ).apply {
-                interpolator = AnticipateInterpolator()
-                duration = 300L
-                doOnEnd { splashScreenView.remove() }
-                start()
-            }
-        }
-
-        splashScreen.clearOnExitAnimationListener()
-
     }
 }
