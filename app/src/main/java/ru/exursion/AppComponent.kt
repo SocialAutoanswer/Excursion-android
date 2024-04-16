54eb0f4837c08a182c49d4792fbf25a4787fb4f5
Aleksandr Fofanov <a.fofanov@adguard.com>
2024-04-17 02:14:28 +0300

    add missed files

--- app/src/main/java/ru/exursion/AppComponent.kt
+++ app/src/main/java/ru/exursion/AppComponent.kt
@@ -1,7 +1,18 @@
 package ru.exursion
 
 import dagger.Component
+import ru.exursion.routes.ui.ChooseTownFragment
+import javax.inject.Singleton
 
-@Component(modules = [])
+@Singleton
+@Component(modules = [AppModule::class, AppModule.Bind::class])
 interface AppComponent {
+
+    fun inject(fragment: ChooseTownFragment)
+
+    @Component.Builder
+    interface Builder {
+        fun appModule(appModule: AppModule): Builder
+        fun build(): AppComponent
+    }
 }
--- app/src/main/java/ru/exursion/AppModule.kt
+++ app/src/main/java/ru/exursion/AppModule.kt
@@ -1,9 +1,32 @@
 package ru.exursion
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import dagger.multibindings.IntoMap
+import ru.bibaboba.kit.dagger.ViewModelFactory
+import ru.bibaboba.kit.dagger.ViewModelKey
+import ru.exursion.routes.TownsUseCase
+import ru.exursion.routes.vm.ChooseTownViewModel
 
 @Module
 class AppModule {
 
+    @Provides
+    fun provideTownsUseCase(): TownsUseCase = TownsUseCase()
+
+    @Module
+    interface Bind {
+
+        @Binds
+        fun bindViewModelFactory(impl: ViewModelFactory): ViewModelProvider.Factory
+
+        @Binds
+        @IntoMap
+        @ViewModelKey(ChooseTownViewModel::class)
+        fun bindChooseTownsViewModel(viewModel: ChooseTownViewModel): ViewModel
+    }
 
 }
